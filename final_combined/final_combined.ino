35126e13d207e6be24a9df22f3c7817b6133a0df
Jeevan Shrestha <18102671+jeevan-git@users.noreply.github.com>
2021-06-22 17:20:08 +0545

    Add files via upload

--- final_combined/final_combined.ino
+++ final_combined/final_combined.ino
@@ -4,6 +4,9 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#define count_address 0
+
+uint16_t read_count = 0;
 
 int off_ = 13;
 int switch_ = A0;
@@ -11,7 +14,6 @@ unsigned long last_time;
 unsigned long present_time;
 
 Adafruit_SSD1306 display(128, 32, &Wire, 4);
-int count_ = 0;
 int battery_data = 0;
 void setup() {
 
@@ -36,9 +38,7 @@ void loop() {
   }
 
   else if ((present_time - last_time) > 3000) {
-    count_ ++;
-    EEPROM.update(0, count_);
-//    Serial.println("Inside 8 second");
+    update_count();
     testscrolltext();
     delay(500);
     digitalWrite(off_, HIGH);
@@ -46,7 +46,7 @@ void loop() {
   }
   else {
     delay(500);
-//    Serial.println("Inside the else");
+    //    Serial.println("Inside the else");
     digitalWrite(off_, HIGH);
     delay(2000);
   }
@@ -58,7 +58,7 @@ void testscrolltext(void) {
   display.setTextSize(2); // Draw 2X-scale text
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(10, 1);
-  display.println(count_);
+  display.println(read_count);
   //  display.println(battery_data);
   //  display.println(F(count));
 
@@ -67,13 +67,33 @@ void testscrolltext(void) {
   //  display.startscrollright(0x00, 0x0F);
 }
 
+void update_count() {
+  //  Msb first format
+  uint16_t current_count_0 = EEPROM.read(count_address);
+  uint16_t current_count_1 = EEPROM.read(count_address + 1);
+
+  read_count = (current_count_0 << 8) + current_count_1 + 1;
+
+  if (read_count > 9999) {
+    read_count = 0;
+  }
+
+  EEPROM.update(count_address + 1, read_count & 0x00ff);
+  EEPROM.update(count_address, (read_count >> 8));
+
+}
+
 void initialize_() {
   Serial.begin(9600);
   pinMode(off_, OUTPUT);
   pinMode(switch_, INPUT);
 
+  uint16_t current_count_0 = EEPROM.read(count_address);
+  uint16_t current_count_1 = EEPROM.read(count_address + 1);
+
+  read_count = (current_count_0 << 8) + current_count_1;
+  
   digitalWrite(off_, LOW);
-  count_ = EEPROM.read(0);
   last_time = millis();
   //  EEPROM.update(0, 0);
 }
